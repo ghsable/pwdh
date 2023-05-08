79def8c2c4b795dda97a971f3fae8fdd0baed0fc
dativus <sn.sable005@gmail.com>
2023-05-08 19:16:46 +0900

    update src/Lib.hs - update getPutStr - add guard

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,30 +6,27 @@ import System.Environment (getArgs)            -- base
 import System.Directory (getCurrentDirectory)  -- directory
 
 getPutStr :: (Int, String) -> IO String
-getPutStr (len, opt) =
-  case (len, opt) of
-    (0, _)            -> getCurrentDirectory
-    (1, "-L")         -> return $ "pwdh: no such option: " ++ opt
-    (1, "--logical")  -> return $ "pwdh: no such option: " ++ opt
-    (1, "-P")         -> getCurrentDirectory
-    (1, "--physical") -> getCurrentDirectory
-    (1, "--help")     -> return "Print the full filename of the current working directory.\n\
-                                 \\n\
-                                 \-P, --physical  avoid all symlinks\n\
-                                 \\n\
-                                 \    --help      display this help and exit\n\
-                                 \\n\
-                                 \    --version   output version information and exit\n\
-                                 \\n\
-                                 \If no option is specified, -P is assumed.\n"
-    (1, "--version")  -> return "pwdh 0.3.0.0 created by dativus"
-    (1, _)            -> return $ "pwdh: bad option: " ++ opt
-    _                 -> return "pwdh: too many arguments"
+getPutStr (len, opt)
+  | len == 0              = getCurrentDirectory
+  | len == 2              = return "pwdh: too many arguments"
+  | opt `elem` ["-L", "--logical"]  = return $ "pwdh: no such option: " ++ opt
+  | opt `elem` ["-P", "--physical"] = getCurrentDirectory
+  | opt == "--help"       = return "Print the full filename of the current working directory.\n\
+                                   \\n\
+                                   \-P, --physical  avoid all symlinks\n\
+                                   \\n\
+                                   \    --help      display this help and exit\n\
+                                   \\n\
+                                   \    --version   output version information and exit\n\
+                                   \\n\
+                                   \If no option is specified, -P is assumed.\n"
+  | opt == "--version"    = return "pwdh 0.3.0.0 created by dativus"
+  | otherwise             = return $ "pwdh: bad option: " ++ opt
 
 getOption :: [String] -> IO (Int, String)
 getOption []      = return (0, "")
 getOption [opt]   = return (1, opt)
-getOption (_:_:_) = return (2, "too-many-arguments")
+getOption (_:_:_) = return (2, "too many arguments")
 
 getArgs' :: IO [String]
 getArgs' = getArgs
